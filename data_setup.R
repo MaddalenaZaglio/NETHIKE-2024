16a9ff7f8f2ce85cfbeb3984b9447b976194f3e5
Dylan Lewis <d.lewis.1@research.gla.ac.uk>
2024-09-26 15:53:41 +0100

    add the code to get the simplified network from visone

--- data_setup.R
+++ data_setup.R
@@ -17,4 +17,22 @@ for(i in 1:6){
 potter_1_4 <- from_ties(potter[1:4])
 write_graphml(potter_1_4, "potter_1_4.graphml")
 
-#
+## we simplified in visone, so this reads it back in
+
+potter_fixed <- read.delim("hp4merged.csv", header = T, row.names = 1, sep = ";")
+potter_fixed <- as.matrix(potter_fixed)
+
+# setup for ergm
+
+potter_fixed_net <- network(potter_fixed, directed = TRUE)
+
+potter_attributes <- node_attribute(potter_1_4)
+
+
+
+potter_fixed_net %v% "resistance" <- potter_attributes$resistance
+potter_fixed_net %v% "gender" <- potter_attributes$gender
+potter_fixed_net %v% "house" <- potter_attributes$house
+
+
+
